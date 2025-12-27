30a7ef17ba7368f68710457211aad4653aae8d3a
jthecoder12 <deepusj@gmail.com>
2025-12-27 11:46:24 -0500

    Add file loading to web interpreter

--- webinterpreter/brackets.py
+++ webinterpreter/brackets.py
@@ -4,8 +4,12 @@ import binascii
 brackets_scripts = document.select('script[type="text/brackets"]')
 
 for script in brackets_scripts:
+    if "src" in script.attrs: 
+        with open(script.attrs["src"], "r") as f: code = f.read()
+    else: code = script.text
+
     try:
-        newtypedcode = script.text.replace("(", "0").replace(")", "1")
+        newtypedcode = code.replace("(", "0").replace(")", "1")
         exec(binascii.unhexlify("%x" % int(newtypedcode, 2)).decode("utf-8"))
     except Exception as e:
         print(e)
